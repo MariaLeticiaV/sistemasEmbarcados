9030e3c90ef7092451de4b11e70604c299c9057a
MariaLeticiaV <mlvo@ic.ufal.br>
2025-11-11 00:49:54 -0300

    .

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+static void timer_handler(struct k_timer *t)
+{
+	static uint32_t c;
+	ARG_UNUSED(t);
+	LOG_INF("Hello World!");
+	LOG_DBG("Execução #%u", c);
+	if ((c++ % 10U) == 0U) {
+		LOG_ERR("Erro a cada 10 execuções");
+	}
+}
+
+K_TIMER_DEFINE(timer, timer_handler, NULL);
+
+int main(void)
+{
+	LOG_INF("Iniciando timer com período %d ms", CONFIG_HELLO_TIMER_PERIOD_MS);
+	k_timer_start(&timer, K_NO_WAIT, K_MSEC(CONFIG_HELLO_TIMER_PERIOD_MS));
+	for (;;) k_sleep(K_FOREVER);
+	return 0;
+}
